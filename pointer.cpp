953487279a3e283906cb193f5fd7f4accd0a90bd
talha4t <mahmudtalha80@gmail.com>
2022-12-03 13:02:22 +0600

    pointer

--- pointer.cpp
+++ pointer.cpp
@@ -12,13 +12,27 @@ int main() {
     // cout << ptr << endl;
     // cout << &n << endl;
    
-   
+
     // Size of pointer also data type 
     int b = 5;
     int *p2 = &b;
 
+    double d = 1.2;
+    double *p3 = &d;
+
+    char c = 'a';
+    char *p4 = &c;
+
+    float f = 11.2;
+    float *p5 = &f;
+
     cout << sizeof(b) << endl;
-    cout << sizeof(p2) << endl;
+
+    cout << sizeof(p2) << endl; // size of int pointer 
+    cout << sizeof(p3) << endl; // size of double pointer
+    cout << sizeof(c) << endl; // size of character pointer 
+    cout << sizeof(f) << endl; // size of float pointer
+
 
    
     return 0;
