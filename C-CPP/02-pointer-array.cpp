7af31e34819779f04dbb67df3d3e6d8fe063a357
talha4t <mahmudtalha80@gmail.com>
2022-12-09 18:27:40 +0600

    Update

--- C-CPP/02-pointer-array.cpp
+++ C-CPP/02-pointer-array.cpp
@@ -5,16 +5,18 @@ int main() {
 
     int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    // cout << "address of first memory block is = " << &a[0] << endl;
+    cout << "address of first memory block is = " << &a[0] << endl;
 
     // Printing 0 -> 9 address of this array
-    // for (int i = 0; i < 10; i++)
-    //     cout << &a[i] << " "; 
+    for (int i = 0; i < 10; i++)
+        cout << &a[i] << " "; 
 
     cout << *a + 1 << endl;  // Its print the 0 index value + 1
     cout << *a + 2 << endl;  // Its print the 0 index value + 2
 
     cout << *(a + 1) << endl; // Its gonna print next value after the zero index
     cout << *(a + 5) << endl; // Its gonna print the 6th element
+
+    
     return 0;
 }
