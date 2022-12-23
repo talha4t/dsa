db8451caf65b3ad0b15bfdcc8247603e3675aacb
talha4t <mahmudtalha80@gmail.com>
2022-12-23 15:33:42 +0600

    Update

--- STL/01-Stack.cpp
+++ STL/01-Stack.cpp
@@ -9,12 +9,23 @@ int main() {
     s.push(2); //   : 7 2
     s.push(5); //   : 7 2 5
 
+    // stack size
+    cout << s.size() << '\n';
+
     cout << s.top() << '\n'; //    : 5
 
     s.pop(); //     : 7 2
 
     cout << s.top() << '\n'; //    : 2
 
+    // Checking the stack empty or not
+
+    if (s.empty())
+        cout << "Empty" << '\n';
+    else
+        cout << "Not Empty" << '\n';
+
+    
     s.pop(); //     : 7
 
     cout << s.top() << '\n'; //    : 7
