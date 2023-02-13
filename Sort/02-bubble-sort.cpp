a141a2c283cff62dda7391b48d4c3156d8180ed2
talha4t <mahmudtalha80@gmail.com>
2023-02-13 23:05:06 +0600

    Quick Sort

--- Sort/02-bubble-sort.cpp
+++ Sort/02-bubble-sort.cpp
@@ -3,11 +3,15 @@ using namespace std;
 
 void bubble_sort(int num[], int n) {
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (num[j] > num[j + 1]) {
-                int temp = num[j];
-                num[j] = num[j + 1];
+
+    // 2 3 4 5
+    // 0 1 2 3
+    // 2 3 4 5 (sorted)
+    for (int i = 0; i < n; i++) { // i = 0
+        for (int j = 0; j < n - i - 1; j++) {  // j 0 -> 2
+            if (num[j] > num[j + 1]) { // j = 0
+                int temp = num[j]; // temp = 5
+                num[j] = num[j + 1]; // 
                 num[j + 1] = temp;
             }
         }
