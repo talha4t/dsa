f849b628519a0a6f0cc67c503b40f80505f3b0c6
talha4t <mahmudtalha80@gmail.com>
2023-05-22 12:05:32 +0600

    added lower and upper bound

---
+++ STL/12-Lower-Bound.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    vector<int> v({1, 2, 4, 4, 5, 5, 6});
+
+    int x = 4;
+
+    auto it = lower_bound(v.begin(), v.end(), x);
+
+    cout << (it - v.begin()) << '\n';
+        
+    return 0;
+}
+
+/*
+    author:  talha4t
+    github.com/talha4t (Talha Mahmud)
+    Monday, May 22, 2023 | 12:01:55 (UTC +06:00) Dhaka
+*/
